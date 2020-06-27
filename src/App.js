935360bc58be416d09410d72c0f2bafe998460c6
Ariel Garcia <ga.arielgarcia@gmail.com>
2020-06-27 15:07:35 -0300

    Inicializacion de Create-React-App y primeros ajustes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
+
