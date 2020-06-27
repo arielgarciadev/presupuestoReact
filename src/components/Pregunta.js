74834c75cfb46cd75c1b66639a121542f998d899
Ariel Garcia <ga.arielgarcia@gmail.com>
2020-06-27 15:44:24 -0300

    Validación y creación del componente "Error"

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,45 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
+import Error from './Error'
 
 const Pregunta = () => {
+
+    // Definir el State
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    // Función que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value,10))
+    }
+
+    // Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        // Validar
+        if(cantidad < 1 || isNaN(cantidad)) {
+            guardarError(true)
+            return
+        }
+
+        // Si se pasa la validación
+        guardarError(false)
+    }
+
     return (
         <Fragment>
             <h2>Coloca un presupuesto</h2>
 
-            <form>
+            { error ? <Error mensaje="El presupuesto es incorrecto"/> : null}
+
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input 
                 type="number"
                 className="u-full-width"
                 placeholder="Coloca un presupuesto"
+                onChange={definirPresupuesto}
                 />
 
                 <input 
