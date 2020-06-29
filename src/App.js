a53bc0b54e436ad54c1512d7278751696353fce9
Ariel Garcia <ga.arielgarcia@gmail.com>
2020-06-29 12:31:11 -0300

    Condicional de componentes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
   // Definir el State
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mortrarpregunta, actualizarPregunta] = useState(true);
 
 
   return (
@@ -15,20 +16,25 @@ const App = () => {
         <h1>Gasto Semanal</h1>
 
         <div className="contenido-principal contenido">
-          <Pregunta
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante}
-          />
 
-        <div className="row">
-          <div className="one-half column">
-            <Formulario/>
-          </div>
-          <div className="one-half column">
-            2
-          </div>
-        </div>
-
+          {mortrarpregunta ? 
+            (         
+              <Pregunta
+              guardarPresupuesto={guardarPresupuesto}
+              guardarRestante={guardarRestante}
+              actualizarPregunta={actualizarPregunta}
+              />
+            ) : 
+            (
+              <div className="row">
+                <div className="one-half column">
+                  <Formulario/>
+                </div>
+                <div className="one-half column">
+                  2
+                </div>
+            </div>
+            )}
 
         </div>
       </header>
